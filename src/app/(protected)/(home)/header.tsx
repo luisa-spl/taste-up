ec6b1ebeae695a60ca2276df7b056a79fd234c94
luisa spinola <luisa.spinola@cubos.academy>
2025-06-21 19:21:54 -0300

    chore: improving header

--- src/app/(protected)/(home)/header.tsx
+++ src/app/(protected)/(home)/header.tsx
@@ -1,15 +1,21 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-start p-12 gap-8 absolute top-0 w-full h-32 bg-chart-2">
-      <Avatar className="size-32 -mb-28">
+    <header className="flex items-end justify-start p-12 pl-20 pb-4 gap-8 w-full h-48 bg-[url(/img/sushi-bg.jpg)] bg-cover bg-center">
+      <Avatar className="size-42 -mb-24 border-4 border-background">
         <AvatarImage src="https://github.com/shadcn.png" />
       </Avatar>
-      <nav>
-        <ul className="flex">
-          <li><a href="#">Perfil</a></li>
-          <li><a href="#">Ver cardápio</a></li>
+      <nav className="flex flex-col gap-3 text-3xl font-semibold">
+        <h2>Vip Sushi</h2>
+        <ul className="flex gap-4">
+          <Badge asChild variant="secondary">
+            <li><a href="#" className="text-sm">Perfil</a></li>
+          </Badge>
+          <Badge asChild variant="secondary">
+            <li><a href="#" className="text-sm">Cardápio</a></li>
+          </Badge>
         </ul>
       </nav>
     </header>
