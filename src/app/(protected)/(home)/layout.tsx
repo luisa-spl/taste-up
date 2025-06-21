b6d88a315e851f49d526cce6fc907286e4069e23
luisa spinola <luisa.spinola@cubos.academy>
2025-06-21 19:22:37 -0300

    feat: starting home page

--- src/app/(protected)/(home)/layout.tsx
+++ src/app/(protected)/(home)/layout.tsx
@@ -5,7 +5,7 @@ export default function HomeLayout({ children }: { children: ReactNode}){
   return (
     <>
       <Header />
-      <main>
+      <main className="flex w-full min-h-[50vh] px-4 md:px-8 lg:px-32 py-4 md:py-8 lg:py-12">
         {children}
       </main>
     </>
--- src/app/(protected)/(home)/page.tsx
+++ src/app/(protected)/(home)/page.tsx
@@ -1,6 +1,18 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const hasItems: boolean = false;
+
   return (
-    <div>Hello</div>
+    <>
+      {hasItems ? (
+        <></>
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-4 p-4 min-h-full w-full">
+          <p>Você ainda não adicionou nenhum item ao seu cardápio.</p>
+          <Button>Adicionar item</Button>
+        </div>
+      )}
+    </>
   );
 }
